e46781af80fb8cc6d0d5d6c6e42a2e7876fc75d2
vivek-goli <vivek.goli@kanerika.com>
2024-10-08 17:52:43 +0530

    Initial Commit

---
+++ modules/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, jsonify
+import os
+import traceback
+import logging
+from pathlib import Path
+from ssis_fabric import SSIS_Fabric
+import config
+
+parent_directory = Path(__file__).parent.parent
+log_file_path = parent_directory / config.LOG_FILE
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(log_file_path),
+        logging.StreamHandler()
+    ]
+)
+
+app = Flask(__name__)
+
+@app.route('/migrate', methods=['POST'])
+def migrate():
+    try:
+        dtsx_file = request.files['dtsxFile']
+        workspace_name = request.form['workspaceName']
+        lakehouse_name = request.form['lakehouseName']
+        warehouse_name = request.form['warehouseName']
+        pipeline_name = request.form['pipelineName']
+        endpoint = request.form['endpoint']
+
+        logging.info(f"File received is {dtsx_file.filename}")
+        parent_dir = Path(__file__).resolve().parent.parent
+        temp_dir = parent_dir / config.UPLOAD_FOLDER
+        # Save uploaded file
+        dtsx_file_path = os.path.join(temp_dir, dtsx_file.filename)
+        dtsx_file.save(dtsx_file_path)
+        logging.info("File saved in temp library")
+
+        obj = SSIS_Fabric(workspace_name, lakehouse_name, warehouse_name, pipeline_name, endpoint)
+        obj.create_token()
+        obj.get_workspace_id()
+        obj.get_lakehouse_id()
+        obj.get_warehouse_id()
+        obj.parse_ssis_pipeline(dtsx_file_path)
+
+        encoded = obj.encode_json_to_base64()
+        obj.create_payload_json(pipeline_name, encoded)
+        response = obj.create_pipeline()
+
+        if os.path.exists(dtsx_file_path):
+            os.remove(dtsx_file_path)
+        return response
+
+    except Exception as e:
+        tb_str = traceback.format_exc()
+        print("Traceback details:")
+        print(tb_str)
+        return jsonify({"message": f"Failed to migrate: {str(e)}"}), 500
+
+if __name__ == '__main__':
+    app.run(port=config.PORT, debug=True)
---
+++ modules/config.py
@@ -0,0 +1,18 @@
+UPLOAD_FOLDER = 'temp'
+TEMPLATES_FOLDER = './json_templates/'
+LOG_FILE = 'logs/app.log'
+
+ENVIRONMENT = 'local'
+PORT = '5014'
+
+CLIENT_ID = "c657b643-3d0b-42a2-8680-2d05f60ffef0"
+TENANT_ID = "ea8bd1fd-ac34-4ae2-b421-6cfa2fcff243"
+SCOPE = "https://api.fabric.microsoft.com/.default"
+AUTHORITY = "https://login.microsoftonline.com/"
+API_URL = "https://api.fabric.microsoft.com/v1/"
+USERNAME = "vivek.goli@kanerika.com"
+PASSWORD = "Vivek@16"
+NAMESPACE1 = "www.microsoft.com/SqlServer/Dts"
+NAMESPACE2 = "www.microsoft.com/sqlserver/dts/tasks/sqltask"
+
+FLIP_URL = "http://localhost:8082/importer"
